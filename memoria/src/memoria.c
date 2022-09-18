ce8886e851b1c9fb111ecda334e10143f4028779
Camila Garcia Federico <camilaabigailgf@gmail.com>
2022-09-18 20:26:15 +0000

    leo la config de memoria

--- memoria/src/memoria.c
+++ memoria/src/memoria.c
@@ -5,24 +5,27 @@ t_config* config;
 t_log* logger;
 
 int main(int argc, char ** argv){
+    
     /*if(argc > 1 && strcmp(argv[1],"-test")==0)
         return run_tests();
     else{  
-        logger = log_create("./cfg/memoria.log", "MEMORIA", true, LOG_LEVEL_INFO);
+        
         log_info(logger, "Soy la memoria! %s", mi_funcion_compartida());
         log_destroy(logger);
     }*/
+
+    logger = log_create("./cfg/memoria.log", "MEMORIA", true, LOG_LEVEL_INFO);
 
     //PRUEBAS
     cargar_configuracion(); 
 }
 
 void cargar_configuracion() {
-	config = config_create("archivo_configuracion.config");
-    puts ("arranco a leer la config");
     
-    config_valores.algoritmo_reemplazo = config_get_string_value(config, "ALGORITMO_REEMPLAZO");
-	/*config_valores.entradas_por_tabla = config_get_int_value(config, "ENTRADAS_POR_TABLA");
+	config = config_create("cfg/archivo_configuracion.config");
+    log_info(logger, "Arranco a leer el archivo de configuracion");
+
+	config_valores.entradas_por_tabla = config_get_int_value(config, "ENTRADAS_POR_TABLA");
 	config_valores.marcos_por_proceso = config_get_int_value(config, "MARCOS_POR_PROCESO");
 	config_valores.path_swap = config_get_string_value(config, "PATH_SWAP");
 	config_valores.puerto = config_get_int_value(config, "PUERTO_ESCUCHA");
@@ -30,6 +33,8 @@ void cargar_configuracion() {
 	config_valores.retardo_swap = config_get_int_value(config, "RETARDO_SWAP");
 	config_valores.tam_memoria = config_get_int_value(config, "TAM_MEMORIA");
 	config_valores.tam_pagina = config_get_int_value(config, "TAM_PAGINA");
-    config_valores.tam_pagina = config_get_int_value(config, "TAMANIO_SWAP");*/
-    puts ("se cargo la config");
+    config_valores.tam_pagina = config_get_int_value(config, "TAMANIO_SWAP");
+
+    log_info(logger, "Termino de leer el archivo de configuracion");
+    log_destroy(logger);
 }
