cb3de624f0df200a03a6c7e1c0ad09d52544fe5c
Facundo Azula <azulafacundo@gmail.com>
2022-09-18 21:46:08 +0000

    agrego que tome la config la cpu

--- cpu/include/cpu.h
+++ cpu/include/cpu.h
@@ -3,4 +3,16 @@
 
 #include "shared_utils.h"
 #include "tests.h"
+typedef struct config_cpu {
+	int entradas_tlb;
+	char* reemplazo_tlb;
+	int retardo_instruccion;
+	char* ip_memoria;
+	int puerto_memoria;
+	int puerto_escucha_dispatch;
+	int puerto_escucha_interrupt;
+} config_cpu;
+
+void cargar_configuracion();
+
 #endif
--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -1,5 +1,9 @@
 #include "cpu.h"
 
+config_cpu config_valores;
+t_config* config;
+t_log* logger;
+
 int main(int argc, char ** argv){
     if(argc > 1 && strcmp(argv[1],"-test")==0)
         return run_tests();
@@ -8,4 +12,22 @@ int main(int argc, char ** argv){
         log_info(logger, "Soy la cpu! %s", mi_funcion_compartida());
         log_destroy(logger);
     } 
+    cargar_configuracion(); 
+}
+
+void cargar_configuracion() {
+    
+	config = config_create("cfg/archivo_configuracion.config");
+    log_info(logger, "Arranco a leer el archivo de configuracion");
+
+	config_valores.entradas_tlb = config_get_int_value(config, "ENTRADAS_TLB");
+	config_valores.reemplazo_tlb = config_get_string_value(config, "REEMPLAZO_TLB");
+	config_valores.retardo_instruccion = config_get_int_value(config, "RETARDO_INSTRUCCION");
+    config_valores.ip_memoria = config_get_string_value(config, "IP_MEMORIA");
+	config_valores.puerto_memoria = config_get_int_value(config, "PUERTO_MEMORIA");
+	config_valores.puerto_escucha_dispatch = config_get_int_value(config, "PUERTO_ESCUCHA_DISPATCH");
+	config_valores.puerto_escucha_interrupt = config_get_int_value(config, "PUERTO_ESCUCHA_INTERRUPT");
+
+    log_info(logger, "Termino de leer el archivo de configuracion");
+    log_destroy(logger);
 }
