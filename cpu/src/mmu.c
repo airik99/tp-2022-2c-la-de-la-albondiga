1439ba3aa7a29aaa024e15f1cad8973aad3a2164
Camila Garcia Federico <camilaabigailgf@gmail.com>
2022-11-30 23:52:48 +0000

    comentario

--- cpu/src/mmu.c
+++ cpu/src/mmu.c
@@ -22,7 +22,7 @@ uint32_t traducir_direccion_logica(uint32_t direccion_logica) {
 		log_info(logger, "PID: <%d> - TLB MISS - Segmento: <%d> - Pagina: <%d>\n", pcb_actual->pid, num_segmento, num_pagina); //log obligatorio
 		if(es_direccion_fisica_valida(desplazamiento_segmento, tam_max_segmento)) { 
 		respuesta = esta_en_memoria(num_pagina, num_segmento);
-			if(respuesta == PAGE_FAULT) { //TODO: aca hay que acordarnos de evaluar este cod_op en memoria
+			if(respuesta == -1) { //TODO: aca hay que acordarnos de evaluar este cod_op en memoria
 				log_info(logger, "Page Fault PID: <%d> - Segmento: <%d> - Pagina: <%d>", pcb_actual->pid, num_segmento, num_pagina); //log obligatorio
 				t_generaron_page_fault* generaron_page_fault = malloc(sizeof(t_generaron_page_fault));
 				generaron_page_fault->num_pagina = num_pagina;
@@ -58,9 +58,9 @@ int esta_en_memoria(uint32_t num_pagina, uint32_t num_segmento){
 	t_paquete* paquete = crear_paquete(ESTA_EN_MEMORIA); //TODO: aca hay que acordarnos de evaluar este cod_op en memoria
 	agregar_a_paquete(paquete, num_pagina, sizeof(uint32_t));
 	agregar_a_paquete(paquete, num_segmento, sizeof(uint32_t));
-	enviar_paquete(paquete, socket_servidor_dispatch);
+	enviar_paquete(paquete, conexion_memoria);
 	//aca seria el recv
-	return recibir_respuesta(socket_servidor_dispatch); //esto nos deberia devolver un cod_op (que puede ser: ESTA_EN_MEMORIA ó PAGE_FAULT)
+	return recibir_respuesta(conexion_memoria); //esto nos deberia devolver un cod_op (que puede ser: un marco ó -1)
 }
 
 bool es_direccion_fisica_valida(uint32_t desplazamiento_segmento, uint32_t tam_max_segmento) {
