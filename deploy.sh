2418d1baee31ba587c301388a4c3631fe2a36d3f
Facundo Azula <azulafacundo@gmail.com>
2022-12-16 02:33:00 +0000

    espero sea el ultimo commit del deploy

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,13 @@ sudo make install
 cd $CWD
 echo -e "\n\nHaciendo mkdirsXD...\n\n"
 mkdir "$PWD/memoria/obj"
+mkdir "$PWD/cpu/obj"
+mkdir "$PWD/kernel/obj"
+mkdir "$PWD/consola/obj"
+mkdir "$PWD/shared/obj"
 echo -e "\n\nAhi viene el efecto...\n\n"
 make -C ./memoria
+make -C ./cpu
+make -C ./kernel
+make -C ./consola
 echo -e "\n\nListo maquina!\n\n"
